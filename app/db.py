93ba3fd0caedd4aae814ac474b3423734ec6f4dd
William Tradewell <Wktradewell@gmail.com>
2022-02-01 21:38:12 +0000

    Fun stuff with templates! Mostly getting DB data back to users. completely reworked what little backend we have.git p

---
+++ app/db.py
@@ -0,0 +1,30 @@
+from flask import current_app, g
+import mysql.connector
+
+def get_test():
+    # TODO: Pull connection into a utility Class to reduce repeat code, especially w/ tests
+    connection = mysql.connector.connect(  user='johndoe'
+                                     , password='password'
+                                     , host='localhost'
+                                     , database='employees')
+    cur = connection.cursor()
+    
+    cur.execute("SELECT * FROM employees")
+    string = ""
+    for row in cur:
+        string += str(row[2]) + " " + str(row[3]) + " " + str(row[5]) + '\n\n'
+    cur.close()
+    connection.close()
+    return string
+
+def get_table_list(database):
+    return
+
+def get_table_items(database, table):
+    return
+
+def get_item_fields(database, table, item):
+    return
+    
+def get_all_data(database, table, item, field):
+    return
