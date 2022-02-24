bae318fde5694d152abc82284f256a0ccc39b1d8
William Tradewell <Wktradewell@gmail.com>
2022-02-24 20:27:46 +0000

    Set template page to site root Removed text.txt

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,7 @@ from flask import request
 from . import db
 
 @app.route('/')
-@app.route('/index')
 def index():
-	return "Hello, Flask!"
-
-@app.route('/template')
-def temp():
 	return render_template("template.html.jinja")
 
 @app.route('/linepage')
